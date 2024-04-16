6860617294a7153617cfecea554ad6a245eb4b93
Alice Pinheiro <106347376+AliceSantoi@users.noreply.github.com>
2024-04-16 09:12:06 -0300

    Adição de função
    adicionei uma função do tipo void para pular uma linha simples

--- simulador de compras.projetofinal.cpp
+++ simulador de compras.projetofinal.cpp
@@ -2,6 +2,8 @@
 #include <locale.h>
 #include <math.h>
 
+void linha();
+
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int c=1, qp, i, co, par, qpar;
@@ -9,19 +11,19 @@ int main(){
     printf("=============================================================\n");
 	printf("                  SIMULADOR DE COMPRAS\n");
 	printf("=============================================================\n");
-	printf("Veja a tabela de produtos disponíveis abaixo e seus preços:\n\n");
+	printf("Veja a tabela de produtos disponÃ­veis abaixo e seus preÃ§os:\n\n");
 	
-	printf("|Código do produto | Produto      |   Valor  |\n");
+	printf("|CÃ³digo do produto | Produto      |   Valor  |\n");
 	printf("--------------------------------------------\n");
-	printf("|      1           |MAÇÃ     (und)|   R$2,50 |\n");
+	printf("|      1           |MAÃ‡Ãƒ     (und)|   R$2,50 |\n");
 	printf("|      2           |ABACATE  (und)|   R$8,00 |\n");
 	printf("|      3           |BATATA   (und)|   R$1,50 |\n");
 	printf("|      4           |ARROZ    (und)|   R$4,50 |\n");
-	printf("|      5           |Macarrão (und)|   R$3,50 |\n");
+	printf("|      5           |MacarrÃ£o (und)|   R$3,50 |\n");
 	printf("|      6           |Carne    (und)|   R$15,50|\n");
 	printf("|      7           |Tomate   (und)|   R$1,00 |\n");
 	printf("|      8           |Frango   (und)|   R$8,50 |\n");
-	printf("|      9           |Açúcar   (und)|   R$3,20 |\n");
+	printf("|      9           |AÃ§Ãºcar   (und)|   R$3,20 |\n");
 	printf("|     10           |Goiaba   (und)|   R$0,80 |\n");
 	printf("--------------------------------------------\n\n\n");
 	
@@ -29,105 +31,105 @@ int main(){
 	scanf("%i", &i);
 	while(c<=i){
 	printf("================================\n");
-	printf(" Digite o código do %iº produto: ", c);
+	printf(" Digite o cÃ³digo do %iÂº produto: ", c);
 	scanf("%i", &co);	
 	printf("================================\n\n");
 	
 	switch(co){
 	
 	case 1:
-	printf("Você selecionou o produto MAÇÃ!\n");
-	printf("Digite a quantidade de MAÇÃS que deseja comprar: ");
+	printf("VocÃª selecionou o produto MAÃ‡Ãƒ!\n");
+	printf("Digite a quantidade de MAÃ‡ÃƒS que deseja comprar: ");
 	scanf("%i", &qp);
-	printf("\n");
+	linha();
 		v=qp*2.50;
 		s=s+v;
     break;
     case 2:
-	printf("Você selecionou o produto ABACATE!\n");
+	printf("VocÃª selecionou o produto ABACATE!\n");
 	printf("Digite a quantidade de ABACATES que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*8.00;
 		s=s+v;
 	 break;
 	case 3:
-	printf("Você selecionou o produto BATATA!\n");
+	printf("VocÃª selecionou o produto BATATA!\n");
 	printf("Digite a quantidade de BATATAS que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*1.50;
 		s=s+v;
 	break;
 	case 4:
-	printf("Você selecionou o produto ARROZ!\n");
+	printf("VocÃª selecionou o produto ARROZ!\n");
 	printf("Digite a quantidade de ARROZ que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*4.50;
 		s=s+v;	
 	break;
 		case 5:
-	printf("Você selecionou o produto MACARRÃO!\n");
-	printf("Digite a quantidade de MACARRÃO que deseja comprar: ");
+	printf("VocÃª selecionou o produto MACARRÃƒO!\n");
+	printf("Digite a quantidade de MACARRÃƒO que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*3.50;
 		s=s+v;	
 	break;
 		case 6:
-	printf("Você selecionou o produto CARNE!\n");
+	printf("VocÃª selecionou o produto CARNE!\n");
 	printf("Digite a quantidade de Carne que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*15.50;
 		s=s+v;	
 	break;
 		case 7:
-	printf("Você selecionou o produto TOMATE!\n");
+	printf("VocÃª selecionou o produto TOMATE!\n");
 	printf("Digite a quantidade de TOMATE que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*1.00;
 		s=s+v;	
 	break;
 		case 8:
-	printf("Você selecionou o produto FRANGO!\n");
+	printf("VocÃª selecionou o produto FRANGO!\n");
 	printf("Digite a quantidade de FRANGO que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*8.50;
 		s=s+v;	
 	break;	
 		case 9:
-	printf("Você selecionou o produto AÇÚCAR!\n");
-	printf("Digite a quantidade de AÇÚCAR que deseja comprar: ");
+	printf("VocÃª selecionou o produto AÃ‡ÃšCAR!\n");
+	printf("Digite a quantidade de AÃ‡ÃšCAR que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*3.20;
 		s=s+v;	
 	break;
 		case 10:
-	printf("Você selecionou o produto GOIABA!\n");
+	printf("VocÃª selecionou o produto GOIABA!\n");
 	printf("Digite a quantidade de GOIABA que deseja comprar: ");
 		scanf("%i", &qp);
-	printf("\n");	
+	linha();	
 		v=qp*0.80;
 		s=s+v;	
 	break;
 	default:
 		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-		printf("Código de produto inválido!\n");
+		printf("CÃ³digo de produto invÃ¡lido!\n");
 		printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n");
 		break;
 }
 	c++;	
 }   
    printf("---------------------------------\n");
-   printf("O VALOR TOTAL DA COMPRA É: R$%.2f\n", s);
+   printf("O VALOR TOTAL DA COMPRA Ã‰: R$%.2f\n", s);
    printf("---------------------------------\n\n\n");
    
-   printf("Deseja parcelar sua compara? Digite 1 para SIM e 2 para NÃO: ");
+   printf("Deseja parcelar sua compara? Digite 1 para SIM e 2 para NÃƒO: ");
    scanf("%i", &par);
    printf("\n\n");
    if(par==1){
@@ -137,13 +139,18 @@ int main(){
 
 for(i=0; i<qpar; i++){
 	vpar=  s/qpar;
-	printf("O valor total a pagar na %i° PARCELA É: R$%.2f\n", i+1, vpar);
+	printf("O valor total a pagar na %iÂ° PARCELA Ã‰: R$%.2f\n", i+1, vpar);
 } 
 }
 	else if(par==2){
-		printf("Obrigado pela compra!!\n\nSUA COMPRA TOTAL É: R$%.2f", s);
+		printf("Obrigado pela compra!!\n\nSUA COMPRA TOTAL Ã‰: R$%.2f", s);
 		
 	}
 	
 	return(0);
+}
+
+
+void linha(){
+	printf("\n");
 }
